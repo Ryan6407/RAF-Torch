30b08b2f994595d0edc595b171b40fe558f1639e
Ryan6407 <78325984+Ryan6407@users.noreply.github.com>
2022-11-10 19:17:18 -0600

    Add files via upload

---
+++ raft.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class RaFT(torch.nn.Module):
+    def __init__(self, m, n):
+        super().__init__()
+        self.m = m
+        self.n = n
+
+        self.m_weights = nn.Parameter(
+                torch.tensor([1] * self.m, dtype=torch.float)
+            )
+        self.n_weights = nn.Parameter(
+                torch.tensor([1] * self.n, dtype=torch.float)
+            )
+        
+        torch.nn.init.normal_(self.m_weights)
+        torch.nn.init.normal_(self.n_weights)
+
+    def forward(self, inputs):
+        for i in range(self.m):
+            if i == 0:
+                x = torch.pow(inputs, torch.tensor([i]).to(device)) * self.m_weights[0]
+            else:
+                x += torch.pow(inputs, torch.tensor([i]).to(device)) * self.m_weights[0]
+
+        for i in range(self.n):
+            if i == 0:
+                x2 = torch.pow(inputs, torch.tensor([i]).to(device)) * self.n_weights[0]
+            else:
+                x2 += torch.pow(inputs, torch.tensor([i]).to(device)) * self.n_weights[0]
+
+        x2 = 1 + torch.abs(x2)
+        return x/x2
